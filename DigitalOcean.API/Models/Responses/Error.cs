e90104d44664f2f94fae887aed233f1befa191d1
shahab1363 <shahab1363@yahoo.com>
2019-01-30 17:02:12 -0500

    Add detailed error message

---
+++ DigitalOcean.API/Models/Responses/Error.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace DigitalOcean.API.Models.Responses
+{
+    public class Error
+    {
+        /// <summary>
+        /// error identifier
+        /// </summary>
+        [JsonProperty(PropertyName ="id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Detailed error message
+        /// </summary>
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+    }
+}
