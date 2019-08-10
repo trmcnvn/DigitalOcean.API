aef4c4fd13cedd7ce1ed19f76d2c135c950e06a5
nicholi <nschell@gmail.com>
2019-08-10 02:35:37 -0700

    DomainRecord doc updates

--- DigitalOcean.API/Models/Requests/UpdateDomainRecord.cs
+++ DigitalOcean.API/Models/Requests/UpdateDomainRecord.cs
@@ -16,6 +16,8 @@ namespace DigitalOcean.API.Models.Requests {
 
         /// <summary>
         /// Variable data depending on record type.
+        /// For example, the "data" value for an A record would be the IPv4 address to which the domain will be mapped.
+        /// For a CAA record, it would contain the domain name of the CA being granted permission to issue certificates.
         /// </summary>
         [JsonProperty("data")]
         public string Data { get; set; }
@@ -33,7 +35,9 @@ namespace DigitalOcean.API.Models.Requests {
         public int? Port { get; set; }
 
         /// <summary>
-        /// Time to live for the record, in seconds. If not set, the default value is 1800.
+        /// This value is the time to live for the record, in seconds. This defines the time frame that clients can cache queried information
+        /// before a refresh should be requested. There is a minimum ttl value of 30, unless it is not set. If not set, the default value is
+        /// the value of the SOA record. For SOA records, defines the time to live for purposes of negative caching.
         /// </summary>
         [JsonProperty("ttl")]
         public int? Ttl { get; set; }
