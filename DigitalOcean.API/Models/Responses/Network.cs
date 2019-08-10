2a3fcca4ee90378acf596b7d93d736d6b7794513
nicholi <nschell@gmail.com>
2019-08-10 05:42:24 -0700

    Separate V4 and V6 Interface classes. V6 uses integer for Netmask.

--- DigitalOcean.API/Models/Responses/Network.cs
+++ DigitalOcean.API/Models/Responses/Network.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections.Generic;
 
 namespace DigitalOcean.API.Models.Responses {
-    public class Interface {
+    public class InterfaceV4 {
         public string IpAddress { get; set; }
         public string Netmask { get; set; }
         public string Gateway { get; set; }
         public string Type { get; set; }
     }
 
+    public class InterfaceV6 {
+        public string IpAddress { get; set; }
+        public int Netmask { get; set; }
+        public string Gateway { get; set; }
+        public string Type { get; set; }
+    }
+
     public class Network {
-        public List<Interface> V4 { get; set; }
-        public List<Interface> V6 { get; set; }
+        public List<InterfaceV4> V4 { get; set; }
+        public List<InterfaceV6> V6 { get; set; }
     }
 }
